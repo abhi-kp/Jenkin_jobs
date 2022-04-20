a193f02b3fea25fb6e19e57a610826775c8de873
abhi-kp <58202764+abhi-kp@users.noreply.github.com>
2022-04-20 12:54:50 +0530

    Add files via upload

---
+++ jenkins-jobs/salary.sh
@@ -0,0 +1,12 @@
+echo " Enter the basic :"
+read basic
+dp=$(($basic * 50/100))
+comm=$(($dp+$basic))
+da=$(($comm * 35/100))
+hra=$(($comm * 8/100))
+ma=$(( $comm * 3/100))
+pf=$(($comm * 10/100))
+echo "$basic  $dp  $da  $hra  $ma  $pf"
+salary=$(($basic + $dp + $da + $hra + $ma - $pf))
+
+echo "Salary is Rs.$salary"
---
+++ jenkins-jobs/si.sh
@@ -0,0 +1,7 @@
+read -p "enter princi:  " p
+read -p "enter time:  " t
+read -p "enter rate:  " r
+
+
+s=$((($p $t $r)/100))
+echo " The si is $s "
