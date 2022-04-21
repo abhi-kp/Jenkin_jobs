015f74b5be6d8a8b9688ba64a4f3e6e9f2598c07
abhi-kp <58202764+abhi-kp@users.noreply.github.com>
2022-04-21 16:45:07 +0530

    Update cal.sh

--- cal.sh
+++ cal.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-echo "simple calculator"
+
 sum=0
 i="y"
-echo "enter first number"
-read n1
-echo "enter second number"
-read n2
-while [ $i = "y" ]
-do
-echo "1.Addition"
-echo "2.Subtraction"
-echo "3.Multiplication"
-echo "4.Division"
-echo "Enter choice"
-read ch
+
+n1=$1
+
+
+n2=$2
+ch=$3
 case $ch in
 1)sum=$(echo " $n1 + $n2" | bc -l)
 echo "Addition is =" $sum;;
@@ -25,10 +19,3 @@ echo "Mul is =" $sum;;
 echo "div is =" $sum;;
 *)echo "invalid choice"
 esac
-echo "Do you want to continue"
-read i
-if [ $i != "y" ]
-then
-exit
-fi
-done
